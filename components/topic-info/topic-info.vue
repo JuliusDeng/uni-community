7084b12639c8ed59abefae8bb095e86db3f1dd84
JuliusDeng <1576554007@qq.com>
2020-06-26 17:08:33 +0800

    话题详情页上拉加载功能

---
+++ components/topic-info/topic-info.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<!-- 上面背景图 -->
+		<view class="position-relative">
+			<image :src="info.cover" mode="aspectFill" style="height: 300rpx;filter: blur(6px);" class="w-100"></image>
+		</view>
+		<!-- 顶部三行内容 -->
+		<view class="position-relative bg-white px-2" style="z-index: 10;">
+			<view class="flex">
+				<image :src="info.cover"
+				style="height: 150rpx;width: 150rpx; margin-top: -75rpx;" 
+				class="rounded mr-2"></image>
+				<text class="font-md">#{{info.title}}#</text>
+			</view>
+			<view class="flex align-center font text-secondary mt-1">
+				<text class="mr-2">动态：{{info.news_count}}</text>
+				<text>今日：{{info.today_count}}</text>
+			</view>
+			<view class="text-secondary font-md mb-2">{{info.desc}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: ['info']
+	}
+</script>
+
+<style>
+</style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -211,7 +211,7 @@ var demo = [
     unsupport_count: 2 },
 
   comment_count: 2,
-  share_num: 2 }];var commonList = function commonList() {__webpack_require__.e(/*! require.ensure | components/common/common-list */ "components/common/common-list").then((function () {return resolve(__webpack_require__(/*! @/components/common/common-list.vue */ 59));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var loadMore = function loadMore() {__webpack_require__.e(/*! require.ensure | components/common/load-more */ "components/common/load-more").then((function () {return resolve(__webpack_require__(/*! @/components/common/load-more.vue */ 66));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var _default =
+  share_num: 2 }];var commonList = function commonList() {__webpack_require__.e(/*! require.ensure | components/common/common-list */ "components/common/common-list").then((function () {return resolve(__webpack_require__(/*! @/components/common/common-list.vue */ 71));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var loadMore = function loadMore() {__webpack_require__.e(/*! require.ensure | components/common/load-more */ "components/common/load-more").then((function () {return resolve(__webpack_require__(/*! @/components/common/load-more.vue */ 78));}).bind(null, __webpack_require__)).catch(__webpack_require__.oe);};var _default =
 
 
 
